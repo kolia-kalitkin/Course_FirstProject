b798d9d78c37cf3e12950f1e990d8e005c250d78
kolia-kalitkin <nikolay.kalitkin@gmail.com>
2024-09-01 14:48:06 +0600

    убедимся, что правильные шаблоны - index.html и about.html используются на каждой странице и что они отображают ожидаемое содержимое Index и О нас

--- Course_FirstProject/urls.py
+++ Course_FirstProject/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for Course_FirstProject project.
+URL configuration for Cource_FirstProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from django.views.generic import TemplateView
-from blog import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', TemplateView.as_view(template_name='blog/about.html', extra_context={'header': 'О сайте'})),
-    path('contact/', TemplateView.as_view(template_name='blog/contact.html')),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
 ]
